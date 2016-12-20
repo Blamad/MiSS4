a0eff12c47a0b563dc79815083f0b22ad7c23f51
Wojciech Kudirka <qdirqa@gmail.com>
2016-12-20 23:37:50 +0100

    Calculate step based on requested accuracy

--- MiSS4/Main.cpp
+++ MiSS4/Main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include "Gnuplot.h"
 
@@ -8,19 +8,37 @@ string FILE_PATH = "plot.dat";
 int MIN_X = 0;
 int MAX_X = 1;
 
-double H = 0.1;
 double DEFAULT_Y0 = 3;
 
-double fun(double x);
+double fun(double x, double y);
 void calculateRK3(double y0, double e);
 void drawResult();
 
-double fun(double x, double y)
+int main(int argc, char* argv[])
 {
-	return -2*y;
+	double y0, epsilon, h;
+
+	if (argc != 3)
+	{
+		cout << "Niewystarczajaca ilosc parametrow! Uzywam domyslnych.\n";
+		y0 = DEFAULT_Y0;
+		epsilon = 0.0001;
+	}
+	else
+	{
+		y0 = atoi(argv[1]);
+		epsilon = atoi(argv[2]);
+	}
+
+	//http://www.ikb.poznan.pl/almamater/wyklady/metody_komputerowe_03-04/03.pdf
+	//błąd aproksymacji - Ο(h^4)
+	h = pow(epsilon, 1 / 4.0);
+
+	calculateRK3(y0, h);
+	drawResult();
 }
 
-void calculateRK3(double y0, double epsilon)
+void calculateRK3(double y0, double h)
 {
 	ofstream resultFile;
 	resultFile.open(FILE_PATH);
@@ -30,65 +48,37 @@ void calculateRK3(double y0, double epsilon)
 
 	resultFile << x << " " << y << "\n";
 
-	for (x = MIN_X+H; x <= MAX_X; x+=H)
+	for (x = MIN_X + h; x <= MAX_X; x += h)
 	{
 		/*Zrodla:
 		http://www.ikb.poznan.pl/almamater/wyklady/metody_komputerowe_03-04/03.pdf - wzor
 		http://lpsa.swarthmore.edu/NumInt/NumIntFourth.html - sposob implementacji
 		*/
 
-		double K1 = H * fun(x, y);
-		double K2 = H * fun((x + H/2), (y + K1/2));
-		double K3 = H * fun((x + H), (y - K1 + 2*K2));
-		y = y + ((K1 + 4*K2 + K3) / 6);
-		
+		double K1 = h * fun(x, y);
+		double K2 = h * fun((x + h / 2), (y + K1 / 2));
+		double K3 = h * fun((x + h), (y - K1 + 2 * K2));
+		y = y + ((K1 + 4 * K2 + K3) / 6);
+
 		resultFile << x << " " << y << "\n";
 	}
-	
+
 	resultFile.close();
 }
 
-void calculateeExample(double x0, double epsilon)
+double fun(double x, double y)
 {
-	ofstream resultFile;
-	resultFile.open(FILE_PATH);
-
-	for (int x = MIN_X; x <= MAX_X; x += H)
-	{
-		resultFile << x << " " << x << "\n";
-	}
-
-	resultFile.close();
+	return -2 * y;
 }
 
 void drawResult()
 {
 	Gnuplot gp;
-	gp("set xrange ["+to_string(MIN_X)+":"+to_string(MAX_X)+"]");
-	gp("set xlabel \"O� X\"");
-	gp("set ylabel \"O� Y\"");
+	gp("set xrange [" + to_string(MIN_X) + ":" + to_string(MAX_X) + "]");
+	gp("set xlabel \"Oś X\"");
+	gp("set ylabel \"Oś Y\"");
 	gp("set title \"Wykres z pliku\"");
 	gp("set key top left");
-	gp("plot \"" + FILE_PATH + "\" index 0:0 using 1:2 title \"f(x)\" with linespoints" );
+	gp("plot \"" + FILE_PATH + "\" index 0:0 using 1:2 title \"f(x)\" with linespoints");
 	system("pause");
-}
-
-int main(int argc, char* argv[])
-{
-	double y0, epsilon;
-
-	if (argc != 3)
-	{
-		cout << "Niewystarczajaca ilosc parametrow! Uzywam domyslnych.\n";
-		y0 = DEFAULT_Y0;
-		epsilon = 1;
-	}
-	else
-	{
-		y0 = atoi(argv[1]);
-		epsilon = atoi(argv[2]);
-	}
-
-	calculateRK3(y0, epsilon);
-	drawResult();
 }
